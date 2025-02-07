39641188b818dae9afaa30c3212450ee7030288d
ryanbenilde <ryan.azur@benilde.edu.ph>
2025-02-07 17:05:12 +0800

    JS Lectures

---
+++ 01-variable-datatypes/10challenge1-number/script.js
@@ -0,0 +1,11 @@
+const myString = 'developer';
+
+// const myNewString = myString.charAt(0).toUpperCase() + myString.substring(1)
+
+// const myNewString = myString[0].toUpperCase() + myString.substring(1)
+
+// const myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`
+
+console.log(myNewString); // 'Developer'
+
+
